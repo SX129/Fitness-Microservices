2ce916308783164b95d8ee5f02bb279065ae42de
Henry Nguyen <nguyenhenry682@gmail.com>
2025-04-20 16:22:58 -0500

    initial commit for activity ms

---
+++ activityms/src/main/java/com/fitness/activityms/ActivitymsApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.activityms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitymsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitymsApplication.class, args);
+	}
+
+}
---
+++ activityms/src/test/java/com/fitness/activityms/ActivitymsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.activityms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitymsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
