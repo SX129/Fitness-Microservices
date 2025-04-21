ff81ab03870c5912ee66269f938eb2e27a8b6822
Henry Nguyen <nguyenhenry682@gmail.com>
2025-04-20 21:50:51 -0500

    disables _class in db entry

--- activityms/src/main/java/com/fitness/activityms/config/MongoConfig.java
+++ activityms/src/main/java/com/fitness/activityms/config/MongoConfig.java
@@ -1,9 +1,26 @@
 package com.fitness.activityms.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.*;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @Configuration
 @EnableMongoAuditing
 public class MongoConfig {
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(
+            MongoDatabaseFactory factory,
+            MongoCustomConversions conversions,
+            MongoMappingContext context
+    ) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, context);
+        converter.setCustomConversions(conversions);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null)); // disables _class
+        return converter;
+    }
 }
